f81b953b3aa96cc3f50ed6c55e0c1a53221470a9
olowo-elisha-ibukun <elishaibukunolowo@gmail.com>
2026-06-03 15:08:57 +0100

    fix: streamline db connection pool and remove startup file constraints

--- database/db.js
+++ database/db.js
@@ -1,8 +1,5 @@
 ﻿import dotenv from 'dotenv';
 import pkg from 'pg';
-import { readFile } from 'fs/promises';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
 dotenv.config();
 
@@ -18,16 +15,5 @@ const pool = new Pool({
     connectionString,
     ssl: useSsl ? { rejectUnauthorized: false } : false
 });
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const setupSqlPath = new URL('../setup.sql', import.meta.url);
-const setupSql = await readFile(setupSqlPath, 'utf8');
-
-try {
-    await pool.query(setupSql);
-} catch (error) {
-    console.error('Database schema initialization failed:', error);
-    throw error;
-}
 
 export default pool;
